2d1ee0daba5c87a21c28235babb33996d2ecbb03
George <ugulava.george@gmail.com>
2016-05-27 01:37:30 +0300

    initial commit

---
+++ src/DataManager/Preprocessor.java
@@ -0,0 +1,11 @@
+package DataManager;
+
+import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.tokenize.Tokenizer;
+
+/**
+ * Created by George on 27.05.2016.
+ */
+public class Preprocessor {
+
+}
